9c3c5f25c3c937e2bcccfd927301202b8db12965
Alvin <alvin.liang@mail.utoronto.ca>
2022-06-21 16:56:29 -0400

    completed Invert Binary Tree

---
+++ 6InvertingBinaryTree.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        //the base case 
+        if (root == Null){
+            return Null;
+        }
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+}
